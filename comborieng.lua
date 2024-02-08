b20c69ae21488569cc3e1ee25c99043854287cea
ErutTheTeru <143543521+ErutTheTeru@users.noreply.github.com>
2024-02-08 19:38:42 +0700

    Update comborieng.lua

--- comborieng.lua
+++ comborieng.lua
@@ -1,1 +1,40 @@
-
+getgenv().Setting = {
+    ["Team"] = "Pirates",
+    ["Chat"] = {"Auto Bounty From Rimus Hub"},
+    ["Skip Race V4"] = true,
+    ["Misc"] = {
+        ["Lock Bounty"] = {0, 300000000},
+        ["Hide Health"] = {4000,5000},
+        ["Lock Camera"] = true,
+        ["Enable Cam Farm"] = false,
+        ["White Screen"] = false, 
+        ["FPS Boost"] = false,
+        ["White Screen"] = false,
+        ["Bypass TP"] = true, 
+        ["Spam All Skill On V4"] = true, 
+        ["Random & Store Fruit"] = true
+    },
+    ["Item"] = {
+        ["Melee"] = {["Enable"] = true,
+            ["Z"] = {["Enable"] = true, ["Hold Time"] = 1.5},
+            ["X"] = {["Enable"] = true, ["Hold Time"] = 0.1},
+            ["C"] = {["Enable"] = true, ["Hold Time"] = 0.1}
+        },
+        ["Blox Fruit"] = {["Enable"] = false,
+            ["Z"] = {["Enable"] = true, ["Hold Time"] = 1.5},
+            ["X"] = {["Enable"] = true, ["Hold Time"] = 0},
+            ["C"] = {["Enable"] = true, ["Hold Time"] = 0},
+            ["V"] = {["Enable"] = true, ["Hold Time"] = 0},
+            ["F"] = {["Enable"] = true, ["Hold Time"] = 0}
+        },
+        ["Sword"] = {["Enable"] = true,
+            ["Z"] = {["Enable"] = true, ["Hold Time"] = 1},
+            ["X"] = {["Enable"] = true, ["Hold Time"] = 0}
+        },
+        ["Gun"] = {["Enable"] = true,
+            ["Z"] = {["Enable"] = true, ["Hold Time"] = 0.1},
+            ["X"] = {["Enable"] = true, ["Hold Time"] = 0.1}
+        } 
+    } 
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Dextral-Code/lua/main/bounty_v2"))()
