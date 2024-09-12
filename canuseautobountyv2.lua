f9c6f2c4303a2bc19648b4cba8782489bcf5bede
Dextral-Code <157484652+Dextral-Code@users.noreply.github.com>
2024-09-12 20:00:49 +0700

    Update canuseautobountyv2.lua

--- canuseautobountyv2.lua
+++ canuseautobountyv2.lua
@@ -1,5 +1,5 @@
 return {
-    bounty = {canuse = true}, 
-    bountyhavekey = {canuse = true}, 
-    main = {canuse = true}
+    bounty = {canuse = true, kick = "join discord: discord.gg/pdKhtkYRqU to get newest script"}, 
+    bountyhavekey = {canuse = true, kick = "discontinued, discord.gg/pdKhtkYRqU"}, 
+    main = {canuse = true, kick = "not able to use"}
 }
